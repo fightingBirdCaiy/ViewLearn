6d94d173d6676732eb4e5e7b984c94d9ced030dc
IQIYI\caiyong <caiyong@qiyi.com>
2020-05-26 19:21:22 +0800

    no message

--- app/src/main/java/com/caiy/view/learn/view/custom/XfermodeView.java
+++ app/src/main/java/com/caiy/view/learn/view/custom/XfermodeView.java
@@ -3,6 +3,9 @@ package com.caiy.view.learn.view.custom;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -16,6 +19,7 @@ import androidx.annotation.Nullable;
 public class XfermodeView extends View {
 
     private Paint mPaint = new Paint();
+    private RectF mRectF = new RectF();
 
     public XfermodeView(Context context) {
         this(context, null);
@@ -37,7 +41,8 @@ public class XfermodeView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        testMethod1(canvas);
+//        testMethod1(canvas);
+        testMethod2(canvas);
     }
 
     private void testMethod1(Canvas canvas) {
@@ -47,10 +52,33 @@ public class XfermodeView extends View {
         int canvasWidth = 800;
         int r = canvasWidth / 3;
         //绘制黄色的圆形
-        mPaint.setColor(0xFFFFCC44);
+        mPaint.setColor(0x7FFFCC44);
         canvas.drawCircle(r, r, r, mPaint);
         //绘制蓝色的矩形
-        mPaint.setColor(0xFF66AAFF);
+        mPaint.setColor(0x7F66AAFF);
         canvas.drawRect(r, r, r * 2.7f, r * 2.7f, mPaint);
+    }
+
+    private void testMethod2(Canvas canvas) {
+        //设置背景色
+//        canvas.drawARGB(255, 139, 197, 186);
+
+        int canvasWidth = 800;
+        int r = canvasWidth / 3;
+        //绘制黄色的圆形
+        mPaint.setColor(0x7FFFCC44);
+        canvas.drawCircle(r, r, r, mPaint);
+
+        //使用CLEAR作为PorterDuffXfermode绘制蓝色的矩形
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC));
+        //绘制蓝色的矩形
+        mPaint.setColor(0x7F66AAFF);
+
+        mRectF.set(r, r, r * 2.7f, r * 2.7f);
+        canvas.drawRoundRect(mRectF, 50, 50, mPaint);
+//        canvas.drawRect(mRectF, mPaint);
+
+        //最后将画笔去除Xfermode
+        mPaint.setXfermode(null);
     }
 }
